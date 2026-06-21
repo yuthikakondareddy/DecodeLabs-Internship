e9eee0e5c125b8220a1cc76fa547fccf79c452ca
yuthikakondareddy <kondareddysriyuthika@gmail.com>
2026-06-21 10:45:22 +0530

    Add files via upload

---
+++ Project3/project3.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.cluster import KMeans
+
+df = pd.read_csv("Mall_Customers.csv")
+
+print(df.head())
+print(df.shape)
+
+print("\nDataset Information:")
+df.info()
+
+print("\nStatistical Summary:")
+print(df.describe())
+
+print("\nMissing Values:")
+print(df.isnull().sum())
+
+X = df[["Annual Income (k$)", "Spending Score (1-100)"]]
+
+wcss = []
+
+for i in range(1, 11):
+    kmeans = KMeans(
+        n_clusters=i,
+        init="k-means++",
+        random_state=42
+    )
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+
+plt.figure(figsize=(8, 5))
+plt.plot(range(1, 11), wcss, marker="o")
+plt.title("Elbow Method")
+plt.xlabel("Number of Clusters")
+plt.ylabel("WCSS")
+plt.show()
+
+kmeans = KMeans(
+    n_clusters=5,
+    init="k-means++",
+    random_state=42
+)
+
+y_pred = kmeans.fit_predict(X)
+
+plt.figure(figsize=(8, 6))
+
+plt.scatter(
+    X.iloc[:, 0],
+    X.iloc[:, 1],
+    c=y_pred
+)
+
+plt.scatter(
+    kmeans.cluster_centers_[:, 0],
+    kmeans.cluster_centers_[:, 1],
+    s=200,
+    marker="X"
+)
+
+plt.title("Customer Segments")
+plt.xlabel("Annual Income (k$)")
+plt.ylabel("Spending Score (1-100)")
+plt.show()
